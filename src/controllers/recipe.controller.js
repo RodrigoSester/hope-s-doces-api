8aef7bb179c8377e77b7130be9fb0018c3c87ba4
Rodrigo <rsesterheim@maisaedu.com.br>
2023-12-22 11:56:32 -0300

    feat(recipe-controller): get weight, parse cost

--- src/controllers/recipe.controller.js
+++ src/controllers/recipe.controller.js
@@ -3,17 +3,22 @@ const { spoonWeightEnum } = require("../enum");
 function _calculateCostOfOne(ingredient) {
   switch (ingredient.type) {
     case 'volume':
-      return ingredient.cost / ingredient.quantity;
+      return (ingredient.cost / ingredient.total_volume) * ingredient.quantity;
     case 'weight':
-      return ingredient.cost / ingredient.total_weight;
+      return (ingredient.cost / _getWeightNumber(ingredient.total_weight)) * _getWeightNumber(ingredient.weight);
     case 'unit':
       return ingredient.cost * ingredient.quantity;
       case 'spoon':
         const spoon = ingredient?.spoon_weight ? spoonWeightEnum[ingredient?.spoon_weight] : spoonWeightEnum.TABLE_FLAT;
-        return ingredient.cost / (ingredient.total_spoons * spoon);
+        return ((ingredient.spoons * spoon) * ingredient.cost) / _getWeightNumber(ingredient.total_weight);
     default:
       return 0;
   }
+}
+
+function _getWeightNumber(weight) {
+  const number = parseFloat(weight);
+  return isNaN(number) ? 0 : number;
 }
 
 const calculateProfit = (req, res) => {
@@ -33,12 +38,14 @@ const calculateProfit = (req, res) => {
     recipe_cost += cost_of_ingredient;    
   }
 
+  recipe_cost = parseFloat(recipe_cost).toFixed(2);
+
   const cost_of_one = recipe_cost / quantity;
   
   const work_value = total_time * hour_value;
 
   const revenue = product_price * quantity
-  const profit = revenue - recipe_cost + work_value;
+  const profit = revenue - (parseFloat(recipe_cost) + work_value);
 
   res.json({
     recipe_cost,
