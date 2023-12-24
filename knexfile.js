593b04118bdc862003d703fb0912673eb816a2e3
Rodrigo <rsesterheim@maisaedu.com.br>
2023-12-24 19:03:46 -0300

    feat(database): config knex

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+};
