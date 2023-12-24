88dba3ff438fd55db15b85183d78065e9e5e828a
Rodrigo <rsesterheim@maisaedu.com.br>
2023-12-24 19:07:35 -0300

    fix(person): chhange table

--- database/migrations/20231224220414_alter_person.js
+++ database/migrations/20231224220414_alter_person.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  return knex.schema.alterTable('user', table => {
+  return knex.schema.alterTable('person', table => {
     table.foreign('created_by').references('id').inTable('user');
     table.foreign('updated_by').references('id').inTable('user');
     table.foreign('deleted_by').references('id').inTable('user');
@@ -16,7 +16,7 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  return knex.schema.alterTable('user', table => {
+  return knex.schema.alterTable('person', table => {
     table.dropForeign('created_by');
     table.dropForeign('updated_by');
     table.dropForeign('deleted_by');
