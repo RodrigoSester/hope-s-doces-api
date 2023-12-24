663501ef77e4df5d007f0cbca93bd09b32f34eb9
Rodrigo <rsesterheim@maisaedu.com.br>
2023-12-24 19:05:59 -0300

    feat(person): set foreign keys

---
+++ database/migrations/20231224220414_alter_person.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('user', table => {
+    table.foreign('created_by').references('id').inTable('user');
+    table.foreign('updated_by').references('id').inTable('user');
+    table.foreign('deleted_by').references('id').inTable('user');
+  })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('user', table => {
+    table.dropForeign('created_by');
+    table.dropForeign('updated_by');
+    table.dropForeign('deleted_by');
+  })
+};
