e7d9f0b64c878b7af8687c182a6e485410f15a7b
Rodrigo <rsesterheim@maisaedu.com.br>
2024-01-11 21:19:03 -0300

    feat(migrations): create order table

---
+++ database/migrations/20240112000854_create_order.js
@@ -0,0 +1,33 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('order', table => {
+    table.increments('id').primary();
+
+    table.integer('person_id').notNullable();
+    table.foreign('person_id').references('id').inTable('person');
+
+    table.text('description').notNullable();
+    table.number('value').notNullable();
+    table.boolean('is_paid').notNullable().defaultTo(false);
+    table.string('adress', 150).notNullable();
+
+    table.integer('created_by').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    
+    table.integer('updated_by').notNullable();
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    
+    table.boolean('is_deleted').defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('order');
+};
