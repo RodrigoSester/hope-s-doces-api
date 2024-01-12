3634879f99cb1870b8fac50d4e6368107160a909
Rodrigo <rsesterheim@maisaedu.com.br>
2024-01-11 21:01:52 -0300

    feat(person): use authorizer middleware

--- src/routes/_person.js
+++ src/routes/_person.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const routes = express.Router();
 
+const authorizer = require('../middlewares/authorizer.middleware');
+
 const controller = require('../controllers/person.controller');
 
-routes.post('/person', controller.registerPerson);
+routes.post('/person', authorizer.verify, controller.registerPerson);
 
 module.exports = routes;
