392f11195e5a99b56383896b6c9ee36cdcbc51ac
Rodrigo <rsesterheim@maisaedu.com.br>
2024-01-18 16:13:29 -0300

    feat(helper): implement existence verifier helper

---
+++ src/helpers/verify-existence.helper.js
@@ -0,0 +1,15 @@
+const { personService } = require('../services');
+
+const personExists = async (person_id) => {
+  const person = await personService.getById(person_id);
+
+  if (!person) {
+    throw new Error('Person not found');
+  }
+
+  return person;
+}
+
+module.exports = {
+  personExists
+}
