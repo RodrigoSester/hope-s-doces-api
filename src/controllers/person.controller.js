a86b34f626cd2bc4d105b926627187d7b9ee6698
Rodrigo <rsesterheim@maisaedu.com.br>
2024-01-18 14:14:12 -0300

    feat(person): get user id from request

--- src/controllers/person.controller.js
+++ src/controllers/person.controller.js
@@ -2,6 +2,7 @@ const { personService } = require('../services');
 
 const registerPerson = async (req, res) => {
   const { name, number } = req.body;
+  const { id } = req.user;
 
   if (!name || !number) {
     return res.status(400).json({
@@ -12,7 +13,8 @@ const registerPerson = async (req, res) => {
   try {
     const personDTO = {
       name,
-      number
+      number,
+      created_by: id
     };
   
     const registeredPerson = await personService.register(personDTO);
--- src/services/_person.service.js
+++ src/services/_person.service.js
@@ -4,10 +4,12 @@ const register = async (person) => {
   return await db
     .insert({
       name: person.name,
-      number: person.number
+      number: person.number,
+      created_by: person.created_by,
+      updated_by: person.created_by
     })
     .into('person')
-    .returning(['id', 'name', 'number', 'created_at']);
+    .returning(['id', 'name', 'number', 'created_by', 'created_at']);
 };
 
 module.exports = {
