8916920a71ff387cfc2bc646048561df3a662528
Rodrigo <rsesterheim@maisaedu.com.br>
2024-01-18 16:14:09 -0300

    feat(order): trycatch statement, use helper to verify if person exists

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,20 +1,26 @@
 const { orderService } = require('../services');
+const verifyExistence = require('../helpers/verify-existence.helper');
 
 const registerOrder = async (req, res) => {
-  const { person_id, description, value, is_paid, adress } = req.body
-  const { id } = req.user;
-
-  // TODO: verify if person_id exists
-  const orderDTO = {
-    person_id, 
-    description, 
-    value, 
-    is_paid, 
-    adress,
-    created_by: id
+  try {
+    const { person_id, description, value, is_paid, adress } = req.body
+    const { id } = req.user;
+  
+    await verifyExistence.personExists(person_id);
+  
+    const orderDTO = {
+      person_id, 
+      description, 
+      value, 
+      is_paid, 
+      adress,
+      created_by: id
+    }
+    const registeredOrder = await orderService.register(orderDTO);
+    return res.status(201).json(registeredOrder);
+  } catch (err) {
+    throw err;
   }
-  const registeredOrder = await orderService.register(orderDTO);
-  return res.status(201).json(registeredOrder);
 };
 
 module.exports = {
