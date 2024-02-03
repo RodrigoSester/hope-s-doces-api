3c9e200106892119a9466007ac60bd72cb8081bf
Rodrigo <rsesterheim@maisaedu.com.br>
2024-02-03 00:13:51 -0300

    feat(database): get client

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
-const knex = require('knex');
+const Knex = require('knex').Client;
 const knexfile = require('../knexfile');
 const env = process.env.NODE_ENV || 'development';
 const configOptions = knexfile[env];
 
-module.exports = knex(configOptions);
+module.exports = new Knex(configOptions);
