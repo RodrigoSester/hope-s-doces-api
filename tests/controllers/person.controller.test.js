23cb8dab9f0e7b93afb725e7c2c957ef6b3a242d
Rodrigo <rsesterheim@maisaedu.com.br>
2024-02-24 17:14:23 -0300

    feat(person-test): add test for registering a person

--- tests/controllers/person.controller.test.js
+++ tests/controllers/person.controller.test.js
@@ -25,4 +25,21 @@ describe('registerPerson', () => {
     expect(response.status).toBe(400);
     expect(response.body).toEqual({ message: 'Missing required fields' });
   });
+
+  it('should return 201 if the person is successfully registered', async() => {
+    const response = await request(app)
+      .post('/person')
+      .send({
+        name: 'John Doe',
+        number: '123456789'
+      })
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('name', 'John Doe');
+    expect(response.body).toHaveProperty('number', '123456789');
+  });
 });
