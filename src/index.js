a84b9912c1255903454eb060b068d576caec7f56
Rodrigo <rsesterheim@maisaedu.com.br>
2024-03-05 20:09:00 -0300

    feat: deploy configurations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
 
-const routes = require('./src/routes');
+const routes = require('./routes');
 
 app.use(express.json());
+
+app.use('/.netlify/functions/api', routes);
 
 routes.configureRoutes(app);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
+
+module.exports.handler = serverless(app);
