49551d8b2608843ff98d3007e67b159a474b8929
Rodrigo <rsesterheim@maisaedu.com.br>
2024-03-05 23:00:40 -0300

    fix: deploy configs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const app = express();
 
-const routes = require('./routes');
+const app = express();
+const cors = require('cors');
+
+const orderRoutes = require('./routes/_order');
+const userRoutes = require('./routes/_user');
+const personRoutes = require('./routes/_person');
+const recipeRoutes = require('./routes/_recipe');
 
 app.use(express.json());
 
-app.use('/.netlify/functions/api', routes);
+app.use(cors());
 
-routes.configureRoutes(app);
+app.use('/.netlify/functions/app/user', userRoutes);
+app.use('/.netlify/functions/app/person', personRoutes);
+app.use('/.netlify/functions/app/recipe', recipeRoutes);
+app.use('/.netlify/functions/app/order', orderRoutes);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
-
+module.exports = app;
 module.exports.handler = serverless(app);
