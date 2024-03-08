a6916d98e84b055a82b18dabe200bebac614092e
Rodrigo <rsesterheim@maisaedu.com.br>
2024-03-07 22:43:54 -0300

    feat(index): configure routes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const { configureRoutes } = require('./routes');
 
 const app = express();
-const cors = require('cors');
-
-const orderRoutes = require('./routes/_order');
-const userRoutes = require('./routes/_user');
-const personRoutes = require('./routes/_person');
-const recipeRoutes = require('./routes/_recipe');
 
 app.use(express.json());
 
 app.use(cors());
 
-app.use(userRoutes);
-app.use(personRoutes);
-app.use(recipeRoutes);
-app.use(orderRoutes);
+configureRoutes(app);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server running on port 3000');
