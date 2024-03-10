7cebcc3c3f72a188eba41a7cf9861479c80947ab
Rodrigo <rsesterheim@maisaedu.com.br>
2024-03-10 03:33:33 -0300

    feat(knexfile): test environment

--- knexfile.js
+++ knexfile.js
@@ -27,6 +27,23 @@ const knex = {
     seeds: {
       directory: './database/seeds'
     }
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.TEST_DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
   }
 };
 
