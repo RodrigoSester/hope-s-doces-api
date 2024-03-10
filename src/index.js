7e307dda7d245d015d6995f833ac2f045aabb8df
Rodrigo <rsesterheim@maisaedu.com.br>
2024-03-10 20:23:58 -0300

    fix(index): don't listen app when running tests

--- src/index.js
+++ src/index.js
@@ -10,8 +10,10 @@ app.use(cors());
 
 configureRoutes(app);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server running on port 3000');
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('Server running on port 3000');
+  });
+}
 
 module.exports = app;
