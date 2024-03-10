719fe0e94a2638eea1932f696d05e7f20045926f
Rodrigo <rsesterheim@maisaedu.com.br>
2024-03-10 02:04:29 -0300

    feat(user): verify if email already exists

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -29,6 +29,12 @@ const register = async(req, res) => {
   try {
     const { username, email, password } = req.body;
 
+    const user = await userService.getUserByEmail(email);
+
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const userDTO = {
