deea4a62bed3634fb01a6824599b12ac0d4283a0
Rodrigo <rsesterheim@maisaedu.com.br>
2024-03-10 02:42:36 -0300

    fix: imports

--- tests/helpers/test.helper.js
+++ tests/helpers/test.helper.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
-const app = require('../../index');
-
 const knex = require('knex');
+
+const app = require('../../src/index');
 const knexfile = require('../../knexfile');
 
 let server;
