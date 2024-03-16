26213d4ec4f8563bb3fb90fff5c5ae1c8225cb05
Rodrigo <rsesterheim@maisaedu.com.br>
2024-03-16 20:12:07 -0300

    feat(login): implement page and layout to login

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-page>
+    <div class="q-pa-md">
+      <q-card>
+        <q-card-section>
+          <q-form @submit="login">
+            <q-input
+              v-model="username"
+              label="Username"
+              outlined
+              class="q-mb-md"
+            />
+            <q-input
+              v-model="email"
+              label="E-mail"
+              outlined
+              class="q-mb-md"
+            />
+            <q-input
+              v-model="password"
+              label="Senha"
+              type="password"
+              outlined
+              class="q-mb-md"
+            />
+            <q-btn
+              type="submit"
+              label="Entrar"
+              color="primary"
+              class="q-mb-md"
+            />
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import authServices from 'src/services/auth.services';
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginPage',
+  data() {
+    return {
+      email: '',
+      password: '',
+      username: ''
+    }
+  },
+  methods: {
+    async login() {
+      const body = {
+        username: this.username,
+        email: this.email,
+        password: this.password
+      }
+      await authServices.register(body)
+    }
+  }
+})
+</script>
