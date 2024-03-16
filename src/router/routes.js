f3415bcb86835968ba84043d8286371b0770e1c9
Rodrigo <rsesterheim@maisaedu.com.br>
2024-03-16 20:13:06 -0300

    feat(routes): add configuration to login route

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,16 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    ],
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/login',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') }
+    ],
+    meta: { requiresAuth: false }
   },
 
   // Always leave this as last one,
