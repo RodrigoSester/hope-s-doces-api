6ba84e54d6835421c7cef2435f01dd31ee06f7a8
Rodrigo <rsesterheim@maisaedu.com.br>
2024-04-08 19:45:52 -0300

    fix: lint fix

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -7,12 +7,11 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'LoginLayout'
-})
+  name: "LoginLayout",
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
