30b340609d8fc4c9a4a9d122fb1bf7a3d9462829
Rodrigo <rsesterheim@maisaedu.com.br>
2024-04-08 20:18:19 -0300

    feat(quasar-config): add extra

--- quasar.config.js
+++ quasar.config.js
@@ -42,7 +42,6 @@ module.exports = configure(function (/* ctx */) {
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
       // 'ionicons-v4',
-      // 'mdi-v5',
       // 'fontawesome-v6',
       // 'eva-icons',
       // 'themify',
@@ -51,6 +50,7 @@ module.exports = configure(function (/* ctx */) {
 
       'roboto-font', // optional, you are not bound to it
       'material-icons', // optional, you are not bound to it
+      'mdi-v5',
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
