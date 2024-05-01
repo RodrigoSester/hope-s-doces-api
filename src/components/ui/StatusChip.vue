3ebdac3b08d9814bf0a973f98b198b52a5d25fac
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-01 02:42:56 -0300

    feat(status-chip): implement an ui component for status chip

---
+++ src/components/ui/StatusChip.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-chip
+    outline
+    square
+    size="16px"
+    class="status-chip"
+    :label="$t(`common.status.${status}`)"
+  />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import statusChipColorsEnum from "src/enums/statusChipColors.enums";
+
+export default defineComponent({
+  name: "StatusChip",
+  props: {
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    getColor() {
+      return statusChipColorsEnum[this.status.toUpperCase()].color;
+    },
+    getTextColor() {
+      return statusChipColorsEnum[this.status.toUpperCase()].textColor;
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.status-chip {
+  font-weight: 500;
+}
+</style>
