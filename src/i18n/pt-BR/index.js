489046bbb72bcb8bcb973d4f100f21318b1e8783
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-01 00:23:27 -0300

    feat(i18n): implement translates for pt-BR

---
+++ src/i18n/pt-BR/index.js
@@ -0,0 +1,28 @@
+export default {
+  title: "Hope's Doces",
+  menu: "Menu",
+  settings: {
+    title: 'Configurações',
+    language: 'Idioma',
+    theme: 'Tema',
+    light: 'Claro',
+    dark: 'Escuro',
+    auto: 'Automático',
+  },
+  common: {
+    table: {
+      noData: 'Nenhum dado encontrado',
+      noResults: 'Nenhum resultado encontrado',
+      rowsPerPage: 'Itens por página',
+      pagination: 'Página {page} de {pages}',
+    },
+    singular: {
+      order: 'Pedido',
+      client: 'Cliente',
+    },
+    plural: {
+      order: 'Pedidos',
+      client: 'Clientes',
+    }
+  }
+}
