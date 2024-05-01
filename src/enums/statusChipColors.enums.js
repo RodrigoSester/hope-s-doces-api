910a27efc7ae273f0306f208e5dfdaaba7d0968d
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-01 02:43:17 -0300

    feat(enums); implement enum for status chip colors

---
+++ src/enums/statusChipColors.enums.js
@@ -0,0 +1,24 @@
+const statusChipColorsEnum = {
+  PENDING: {
+    color: 'warning--base',
+    textColor: 'warning--text',
+    backgroundColor: 'warning'
+  },
+  SUCCESS: {
+    color: 'success--base',
+    textColor: 'success--text',
+    backgroundColor: 'success'
+  },
+  ERROR: {
+    color: 'error--base',
+    textColor: 'error--text',
+    backgroundColor: 'error'
+  },
+  INFO: {
+    color: 'info--base',
+    textColor: 'info--text',
+    backgroundColor: 'info'
+  }
+};
+
+export default statusChipColorsEnum;
