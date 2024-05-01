9f8a5835c0b1cdd17b2b61bc2717762c13319633
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-01 00:23:41 -0300

    fix(i18n): change the locale to en-US

--- src/i18n/en-US/index.js
+++ src/i18n/en-US/index.js
@@ -1,7 +1,28 @@
-// This is just an example,
-// so you can safely delete all default props below
-
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
+  title: "Hope's Doces",
+  menu: "Menu",
+  settings: {
+    title: 'Settings',
+    language: 'Language',
+    theme: 'Theme',
+    light: 'Light',
+    dark: 'Dark',
+    auto: 'Auto',
+  },
+  common: {
+    table: {
+      noData: 'No data found',
+      noResults: 'No results found',
+      rowsPerPage: 'Itens per page',
+      pagination: 'Page {page} of {pages}',
+    },
+    singular: {
+      order: 'Order',
+      client: 'client',
+    },
+    plural: {
+      order: 'Orders',
+      client: 'Clients',
+    }
+  }
 }
