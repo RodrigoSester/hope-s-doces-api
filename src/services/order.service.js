678323e59f7ff90285a9ba1496e5953841cdeb09
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-10 16:29:07 -0300

    fix(order-service): remove set of header

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,14 +1,8 @@
 import { api } from "src/boot/axios"
-import { getToken } from "src/utils/token.utils"
 
 export default {
   async getAll() {
-    const token = getToken();
-    const response = await api.get('/orders', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      }
-    });
+    const response = await api.get('/orders');
     return response.data;
   }
 }
