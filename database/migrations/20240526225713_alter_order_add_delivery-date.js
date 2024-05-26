a75addbd641eafa963e7d39d52f3cad53693658b
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-26 20:02:57 -0300

    feat(order): add delivery date in order table

---
+++ database/migrations/20240526225713_alter_order_add_delivery-date.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('order', table => {
+    table.string('delivery_date', 20);
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('order', table => {
+    table.dropColumn('delivery_date');
+  })
+};
